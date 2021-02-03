5a546db39b967693f96a7a0c0572c22aab4580c7
defolt17 <defolt_17@mail.ru>
2021-02-03 21:22:38 +0300

    Trying to merge akka server and slick db methods.....

--- server/src/main/scala/server/PostgreSQLTest.scala
+++ server/src/main/scala/server/PostgreSQLTest.scala
@@ -1,10 +1,21 @@
 package server
 
+import akka.Done
+import akka.http.scaladsl.server.Directives.{complete, onSuccess}
+import server.JSORequestReader.saveOrder
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import sun.rmi.runtime.Log
+import scala.concurrent.duration.{SECONDS, _}
+
+import java.lang.Error
 import slick.jdbc.H2Profile.api._
 import scala.concurrent.Future
 import scala.concurrent.Await
-import scala.concurrent.duration._
+
+
 import java.sql.SQLException
+import scala.util.{Failure, Success}
 
 case class User(
                        userid: Int,
@@ -24,26 +35,32 @@ class UsersTable(tag: Tag) extends Table[User](tag,"users") {
 
 
 object PostgreSQLTest extends App {
-
-        val db = Database.forConfig("db")
-        def exec [T](action: DBIO[T]): T =
-            Await.result(db.run(action), 2.seconds)
-
-        val users = TableQuery[UsersTable]
-        val query = users.filter(_.username === "Mike")
+    val db = Database.forConfig("db")
+    val users = TableQuery[UsersTable]
+    val insertUserQuery = users += User(1337, "name", "password", "email1338@mail.ru")
+    val resultFuture: Future[Int] = db.run(insertUserQuery)
+    val result = Await.result(resultFuture, 2.seconds)
 
 
-        val newUser = Seq(User(5, "MEGACHAD", "dungeonmaster96", "asd@asd.asd"))
+    // OK I am not crazy but this Await stuff is kinda suss
+    // .result waits for Future to complete but this fcker wont work with onComplete
+    // But .complete will...........
+    // OMG I am so dead........
 
-        val insert: DBIO[Option[Int]] = users ++= newUser
-        val insertAction: Future[Option[Int]] = db.run(insert)
-        val rowCount = Await.result(insertAction, 2.seconds)
-        println(rowCount)
 
-        val updateQuery = users.filter(_.username === "MEGACHAD")
-        exec(updateQuery.map(_.email).update("definetlynotvirgin@gmail.com"))
 
-        val userAction: DBIO[Seq[User]] = users.result
-        val userFuture: Future[Seq[User]] = db.run(userAction)
-        val userResult = Await.result(userFuture, 2.seconds)
+    //    result.onComplete {
+    //        case Success(v) => println("EEEEEEEEEEEEEE", v)
+    //        case Failure(e) => println(e)
+    //        case _ => println("ALL")
+    //    }
+
+    println(result)
+
+    //    result onComplete {
+    //        case Success(posts) => println(posts)
+    //        case Failure(t) => println("An error has occurred: " + t.getMessage)
+    //    }
+
+
 }
--- server/src/main/scala/server/SNS.scala
+++ server/src/main/scala/server/SNS.scala
@@ -1,69 +1,82 @@
-package server
-
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
-import akka.http.scaladsl.Http
-import akka.Done
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.model.StatusCodes
-import scala.concurrent.Future
-import scala.io.StdIn
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import spray.json.DefaultJsonProtocol._
-
-object SNS extends App{
-
-    implicit val system = ActorSystem(Behaviors.empty, "SprayExample")
-    implicit val executionContext = system.executionContext
-
-    case class User(
-                       userid: Int,
-                       username: String,
-                       password: String,
-                       email: String
-                   )
-
-    var users: List[User] = Nil
-
-    implicit val userFormat = jsonFormat4(User)
-
-    def Register(user: User): Future[Done] = {
-        users = user :: users
-        Future { Done }
-    }
-
-    def GetUser(userid: Int): Future[Option[User]] = Future {
-        users.find(u => u.userid == userid)
-    }
-
-    val route: Route =
-        concat(
-            get {
-                pathPrefix("user" / IntNumber) { userid =>
-                    val maybeUser: Future[Option[User]] = GetUser(userid)
-                    onSuccess(maybeUser) {
-                        case Some(user) => complete(user)
-                        case None       => complete(StatusCodes.NotFound)
-                    }
-                }
-            },
-            post {
-                path("register") {
-                    entity(as[User]) { user =>
-                        val saved: Future[Done] = Register(user)
-                        onSuccess(saved) { _ =>
-                            complete("User registered.")
-                        }
-                    }
-                }
-            }
-        )
-
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-            .flatMap(_.unbind()) // trigger unbinding from the port
-            .onComplete(_ => system.terminate()) // and shutdown when done
-}
+//package server
+//
+//import akka.actor.typed.ActorSystem
+//import akka.actor.typed.scaladsl.Behaviors
+//import akka.http.scaladsl.Http
+//import akka.Done
+//import akka.http.scaladsl.server.Route
+//import akka.http.scaladsl.server.Directives._
+//import akka.http.scaladsl.model.StatusCodes
+//
+//import scala.concurrent.Future
+//import scala.io.StdIn
+//import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+//import server.PostgreSQLTest.{db, newUser, users}
+//import spray.json.DefaultJsonProtocol._
+//import slick.jdbc.H2Profile.api._
+//import slick.jdbc.H2Profile.api._
+//
+//import scala.concurrent.Future
+//import scala.concurrent.Await
+//import scala.concurrent.duration._
+//import java.sql.SQLException
+//import scala.util.{Failure, Success}
+//import server.User
+//import server.UsersTable
+//
+//
+//
+//object SNS extends App{
+//
+//    implicit val system = ActorSystem(Behaviors.empty, "SprayExample")
+//    implicit val executionContext = system.executionContext
+//
+//    val db = Database.forConfig("db")
+//    def exec [T](action: DBIO[T]): T =
+//        Await.result(db.run(action), 2.seconds)
+//    val users = TableQuery[UsersTable]
+//
+//    implicit val userFormat = jsonFormat4(User)
+//
+//    def Register(user: User): Future[Int] = db.run { users += user }
+//
+//    def Register2(newUser: User): Future[Int] = db.run { users += newUser }
+//
+//
+//    def GetUser(userid: Int): Future[Seq[User]] = Future {
+//
+//        val userFilter = users.filter(_.userid === userid).result
+//        val userFuture: Future[Seq[User]] = db.run(userFilter)
+//
+//        Await.result(userFuture, 2.seconds)
+//    }
+//
+//
+//
+//    val route: Route =
+//        concat(
+//            get {
+//                pathPrefix("user" / IntNumber) { userid =>
+//                    val maybeUser: Future[Seq[User]] = GetUser(userid)
+//                    complete(maybeUser)
+//                }
+//            },
+//            post {
+//                path("register") {
+//                    entity(as[User]) { user =>
+//                        Register(newUser) onComplete {
+//                            case Success(user) => complete(user)
+//                            case Failure(t) => complete("Error: ", t)
+//                        }
+//                    }
+//                }
+//            }
+//        )
+//
+//    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+//    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+//    StdIn.readLine() // let it run until user presses return
+//    bindingFuture
+//            .flatMap(_.unbind()) // trigger unbinding from the port
+//            .onComplete(_ => system.terminate()) // and shutdown when done
+//}
